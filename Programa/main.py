a13db8e23f6cc15415002145e4f5ecd7ac7ed5ef
gustavohumanes <reactthefandoms34222682@gmail.com>
2025-07-03 18:46:21 -0300

    Atualizando objetivo

---
+++ Programa/main.py
@@ -0,0 +1,10 @@
+"""
+Queremos classificar os municípios brasileiros em categorias com base na conectividade, por exemplo:
+
+Baixa Conectividade
+Média Conectividade
+Alta Conectividade
+
+Usaremos variáveis como densidade populacional, número de conexões, etc., e nossa variável alvo será uma categoria baseada no Índice de Conectividade.
+"""
+
